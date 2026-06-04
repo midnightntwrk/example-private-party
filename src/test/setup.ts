6897aff7f9f093c262fd686bfa388511106d0be1
Nick Stanford <nstanford5@yahoo.com>
2026-06-04 08:51:41 -0600

    patch fix

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -27,23 +27,11 @@
 // and proving succeeds. We only strip it for binary-body requests so normal
 // (text/JSON) requests are untouched.
 //
+// This is fixed in Undici 7.27.1 -- but there is no Node version that bundles
+// this yet, so this workaround will be in place until there is.
+//
 // Upstream tracking issue:
 // https://github.com/midnightntwrk/servicedesk/issues/38
 
-// const realFetch = globalThis.fetch;
-
-// globalThis.fetch = function patchedFetch(input: RequestInfo | URL, init?: RequestInit) {
-//   const body = init?.body as unknown;
-//   const isBinaryBody =
-//     body != null &&
-//     (body instanceof Uint8Array || ArrayBuffer.isView(body as ArrayBufferView));
-
-//   if (isBinaryBody && init?.headers) {
-//     const headers = new Headers(init.headers as HeadersInit);
-//     if (headers.has('content-length')) {
-//       headers.delete('content-length');
-//       return realFetch(input, { ...init, headers });
-//     }
-//   }
-//   return realFetch(input, init);
-// } as typeof fetch;
+import { fetch as undiciFetch } from 'undici';
+globalThis.fetch = undiciFetch as unknown as typeof fetch;
